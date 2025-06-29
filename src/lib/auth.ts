e849eba57643f35fc23120219b03b388cbc7a10e
StephDev35mg <stephdev35@gmail.com>
2025-06-29 16:03:47 +0300

    Refactor authentication imports: updated paths to use centralized auth module and removed redundant code

---
+++ src/lib/auth.ts
@@ -0,0 +1,46 @@
+import type { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcryptjs"
+
+const prisma = new PrismaClient()
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign-in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"email" | "password", string> | undefined,
+        req: any
+      ): Promise<any> {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        })
+
+        if (user && await bcrypt.compare(credentials?.password ?? '', user.password)) {
+          return { id: user.id, name: user.nameUser, email: user.email }
+        } else {
+          // If you return null then an error will be displayed
+          return null
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+} 
